af1c7df8712b0bd5ba1db0d32a856917e897d141
Aaron <39026927+xaaron3@users.noreply.github.com>
2018-12-08 06:42:45 -0500

    facial detection box added

--- src/App.js
+++ src/App.js
@@ -29,7 +29,27 @@ const particlesOptions = {
 class App extends Component {
   state = {
     input: '',
-    imageUrl: ''
+    imageUrl: '',
+    box: {}
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height)
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box)
+    this.setState({ box: box })
   }
 
   onInputChange = (e) => {
@@ -43,14 +63,8 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
-      .then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
-      },
-      function(err) {
-        // there was an error
-      }
-    );
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
   }
 
   render() {
@@ -65,7 +79,7 @@ class App extends Component {
         <ImageLinkForm 
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './FaceRecognition.css'
 
 const FaceRecognition = (props) => {
    return (
       <div className='center ma'>
          <div className='absolute mt2'>
-            <img alt='' src={props.imageUrl} width='500px' height='auto' />
+            <img id='inputimage' alt='' src={props.imageUrl} width='500px' height='auto' />
+            <div 
+               className='bounding-box' 
+               style={{top: props.box.topRow, right: props.box.rightCol, bottom: props.box.bottomRow, left: props.box.leftCol}}
+            ></div>
          </div>
       </div>
    )
