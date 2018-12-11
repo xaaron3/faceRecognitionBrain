25d5bdebba48dcec4ec0367a93978497e647ec6a
Aaron <39026927+xaaron3@users.noreply.github.com>
2018-12-10 20:06:03 -0500

    nodejs/express api integration

--- src/App.js
+++ src/App.js
@@ -34,7 +34,24 @@ class App extends Component {
     imageUrl: '',
     box: {},
     route: 'signin',
-    isSignedIn: false
+    isSignedIn: false,
+    user: {
+      id: '',
+      name: '',
+      email: '',
+      entries: 0,
+      joined: ''
+    }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
 
   calculateFaceLocation = (data) => {
@@ -59,14 +76,29 @@ class App extends Component {
     this.setState({ input: e.target.value })
   }
 
-  onButtonSubmit = () => {
+  onPictureSubmit = () => {
     this.setState({ imageUrl: this.state.input })
 
     app.models
       .predict(
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => { 
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }))
+            })
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err => console.log(err));
   }
 
@@ -91,16 +123,16 @@ class App extends Component {
         { route === 'home' 
           ? <div> 
               <Logo />
-              <Rank />
+              <Rank name={this.state.user.name} entries={this.state.user.entries} />
               <ImageLinkForm 
                 onInputChange={this.onInputChange}
-                onButtonSubmit={this.onButtonSubmit} />
+                onPictureSubmit={this.onPictureSubmit} />
               <FaceRecognition box={box} imageUrl={imageUrl} />
             </div> 
           : (
             route === 'signin' 
-            ? <SignIn onRouteChange={this.onRouteChange} /> 
-            : <Register onRouteChange={this.onRouteChange} /> 
+            ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} /> 
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} /> 
           )
         }
       </div>
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -15,7 +15,7 @@ const ImageLinkForm = (props) => {
                   onChange={props.onInputChange} />
                <button 
                   className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
-                  onClick={props.onButtonSubmit}
+                  onClick={props.onPictureSubmit}
                   >Detect</button>
             </div>
          </div>
