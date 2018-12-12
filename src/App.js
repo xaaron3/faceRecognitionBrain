624a344c3241af19cd4b145086a3ce83371557ef
Aaron <39026927+xaaron3@users.noreply.github.com>
2018-12-12 02:13:15 -0500

    removing the clarifai api from the front end, sent to server files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 
 import Navigation from './components/Navigation/Navigation';
 import SignIn from './components/SignIn/SignIn';
@@ -12,9 +11,6 @@ import Rank from './components/Rank/Rank';
 
 import './App.css';
 
-const app = new Clarifai.App({
-  apiKey: 'f0f7558592a7428b91be217eeab74239'
- });
 
 const particlesOptions = {
   particles: {
@@ -80,11 +76,14 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({ imageUrl: this.state.input })
-
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL,
-        this.state.input)
+      fetch('http://localhost:3000/imageurl', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          input: this.state.input
+        })
+      })
+      .then(response => response.json())
       .then(response => { 
         if (response) {
           fetch('http://localhost:3000/image', {
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = (props) => {
    return (
       <div>
          <p className='f3'>
-            {'This Magic Brain will detect faces in your pictures. Give it a try!'}
+            {'Enter an image link below and this app will detect a face within the image!'}
          </p>
          <div className='center'>
             <div className='form center pa4 br3 shadow-5'>
